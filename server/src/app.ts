0941c2cb04b729b40132d931ad98c3e80d49ff26
zackdesu <zarchxxx@gmail.com>
2023-12-16 05:41:43 +0700

    using food route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { food } from "./route";
 const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 dotenv.config({
   path: `.env.${app.get("env")}`,
 });
+
+app.use(food);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is online!");
